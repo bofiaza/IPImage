3cfadcd86adce1484e8d80f36fcfb6688f2cd255
Benigno <benigno@apptly.co>
2017-06-01 17:49:49 +0800

    Initial Commit

---
+++ IPImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPImage
+//
+//  Created by Benigno on 6/1/17.
+//  Copyright © 2017 ninjas7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
